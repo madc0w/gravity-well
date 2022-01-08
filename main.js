06c9cb040fc910bc6009fe443dfcd115ccb51609
mad <marc@balloonary.io>
2022-01-08 20:24:47 +0100

    .

--- main.js
+++ main.js
@@ -1,20 +1,64 @@
-let canvas, ctx;
+const massScale = 80;
+const velScale = 0.2;
+let canvas, ctx, center;
+const planets = [];
 
 function onLoad() {
 	canvas = document.getElementById('game-canvas');
 	canvas.width = 800;
 	canvas.height = 800;
-	ctx = canvas.getContext('2d');
-	requestAnimationFrame(draw);
-}
-
-function draw() {
-	const canvasStyle = getComputedStyle(canvas);
-
-	const center = {
+	center = {
 		x: canvas.width / 2,
 		y: canvas.height / 2,
 	};
+	ctx = canvas.getContext('2d');
+
+	planets.push({
+		pos: {
+			x: canvas.width / 2 + 120,
+			y: canvas.height / 2,
+		},
+		vel: {
+			x: 0,
+			y: 6,
+		},
+		mass: 1,
+		style: '#28c',
+	});
+	planets.push({
+		pos: {
+			x: canvas.width / 2 + 240,
+			y: canvas.height / 2,
+		},
+		vel: {
+			x: 0,
+			y: 6,
+		},
+		mass: 2,
+		style: '#66c',
+	});
+
+	requestAnimationFrame(draw);
+
+	setInterval(step, 20);
+}
+
+function step() {
+	for (const planet of planets) {
+		planet.pos.x += planet.vel.x;
+		planet.pos.y += planet.vel.y;
+
+		const _dist = dist(planet.pos, center);
+		planet.vel.x += (velScale * (center.x - planet.pos.x)) / _dist;
+		planet.vel.y += (velScale * (center.y - planet.pos.y)) / _dist;
+	}
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	const canvasStyle = getComputedStyle(canvas);
+
 	const sunRadius = canvas.width / 12;
 
 	const gradient = ctx.createRadialGradient(
@@ -33,5 +77,20 @@ function draw() {
 	ctx.beginPath();
 	ctx.arc(center.x, center.y, sunRadius, 0, 2 * Math.PI);
 	ctx.fill();
+
+	for (const planet of planets) {
+		const r = (planet.mass * canvas.width) / massScale;
+		ctx.fillStyle = planet.style;
+		ctx.beginPath();
+		ctx.arc(planet.pos.x, planet.pos.y, r, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+
 	requestAnimationFrame(draw);
+}
+
+function dist(a, b) {
+	const dx = a.x - b.x;
+	const dy = a.y - b.y;
+	return Math.sqrt(dx * dx + dy * dy);
 }
